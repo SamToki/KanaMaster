01c0374ba58ba2c61a3611422dda5b129d8b2eb3
Sam Toki <SamToki@outlook.com>
2024-01-08 13:04:20 +0800

    I18n adjustment.

--- PROJECT/script.js
+++ PROJECT/script.js
@@ -124,10 +124,6 @@
 			System.I18n.Language = "zh-CN";
 		}
 		switch(System.I18n.Language) {
-			case "zh-CN":
-				/* ChangeCursorOverall("wait");
-				window.location.replace("index.html"); */
-				break;
 			case "en-US":
 				ShowPopupDialog("System_LanguageUnsupported",
 					"Termination",
@@ -139,6 +135,10 @@
 					"Termination",
 					"<span lang='ja-JP'>すみません。このページは日本語にまだサポートしていません。</span>",
 					"", "", "<span lang='ja-JP'>OK</span>");
+				break;
+			case "zh-CN":
+				/* ChangeCursorOverall("wait");
+				window.location.replace("index.html"); */
 				break;
 			case "zh-TW":
 				ShowPopupDialog("System_LanguageUnsupported",
