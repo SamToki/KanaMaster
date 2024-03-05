1b3a04bf32fd525e50998b21945376f959bc9ee3
Sam Toki <SamToki@outlook.com>
2024-03-05 13:57:28 +0800

    Remove "index.html" in codes.

--- PROJECT/script.js
+++ PROJECT/script.js
@@ -1101,7 +1101,7 @@
 			case "System_ConfirmGoToTutorial":
 				switch(Selector) {
 					case 2:
-						window.location.replace("index.html#Item_HelpTutorial");
+						window.location.replace("#Item_HelpTutorial");
 						break;
 					case 3:
 						break;
@@ -1113,7 +1113,7 @@
 			case "System_Error":
 				switch(Selector) {
 					case 2:
-						window.location.replace("index.html#Item_SettingsUserData");
+						window.location.replace("#Item_SettingsUserData");
 						break;
 					case 3:
 						break;
