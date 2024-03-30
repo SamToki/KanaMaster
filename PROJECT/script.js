9838da38607a21bf77ef00f01508c1304e143d8b
Sam Toki <SamToki@outlook.com>
2024-03-30 23:13:05 +0800

    Update script.js

--- PROJECT/script.js
+++ PROJECT/script.js
@@ -1194,7 +1194,7 @@ Automation.GlowStartButton = setInterval(GlowStartButton, 500);
 
 // Error Handling
 function AlertError(Message) {
-	LogCon(Message);
+	LogConsole(Message);
 	ShowDialog("System_Error",
 		"Termination",
 		"抱歉，发生了程序错误。您可尝试清空用户数据以解决问题。是否前往用户数据？",
