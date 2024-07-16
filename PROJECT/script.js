bc9b950985d1aedd5d7e990f4227e872fb422efb
Sam Toki <SamToki@outlook.com>
2024-07-16 18:09:46 +0800

    Update script.js

--- PROJECT/script.js
+++ PROJECT/script.js
@@ -1115,7 +1115,8 @@
 					}
 					break;
 				default:
-					if((System.Display.HotkeyIndicators == "ShowOnWrongKeyPress" && Hotkey.key != "Escape" && Hotkey.key != "Tab" && Hotkey.key != "Enter" && Hotkey.key != " ") || System.Display.HotkeyIndicators == "ShowOnAnyKeyPress" || System.Display.HotkeyIndicators == "AlwaysShow") {
+					if((System.Display.HotkeyIndicators == "ShowOnWrongKeyPress" && IsWrongKeyNegligible(Hotkey.key) == false && Hotkey.key != "F1") ||
+					System.Display.HotkeyIndicators == "ShowOnAnyKeyPress" || System.Display.HotkeyIndicators == "AlwaysShow") {
 						ShowHotkeyIndicators();
 					}
 					break;
