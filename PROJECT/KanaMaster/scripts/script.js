daeab620ef9ff94b261b3ef59af3d85887d0c6ea
Sam Toki <SamToki@outlook.com>
2024-12-05 10:54:03 +0800

    Minor update.

--- PROJECT/KanaMaster/scripts/script.js
+++ PROJECT/KanaMaster/scripts/script.js
@@ -1257,7 +1257,7 @@
 				"}");
 			ShowDialog("System_UserDataExported",
 				"Info",
-				"已导出本页的用户数据至剪贴板。",
+				"已导出本网页的用户数据至剪贴板。",
 				"", "", "", "确定");
 		}
 		function ConfirmClearUserData() {
