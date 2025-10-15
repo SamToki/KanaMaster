5279fb985a2aabdb85358663d7d3081ed96431e7
Sam Toki <SamToki@outlook.com>
2025-10-15 19:34:42 +0800

    v4.04 Minor UI optimization.

--- PROJECT/KanaMaster/script_ServiceWorker.js
+++ PROJECT/KanaMaster/script_ServiceWorker.js
@@ -6,7 +6,7 @@
 	// Declare variables
 	"use strict";
 		// Unsaved
-		const CacheName = "KanaMaster_v4.03";
+		const CacheName = "KanaMaster_v4.04";
 
 // Listeners
 	// Service worker (https://learn.microsoft.com/en-us/microsoft-edge/progressive-web-apps-chromium/how-to/#step-5---add-a-service-worker)
